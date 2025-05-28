708356975efa0d7f2f7afc23a66aa89a59f3a943
RohanY658 <rohanyampati@gmail.com>
2025-05-28 17:54:49 +0530

    Create hellod.js

---
+++ hellod.js
@@ -0,0 +1,6 @@
+// hello.js
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Hello, World!");
+  console.log("Hello, World!222");
+  document.getElementById("hello").innerHTML = "Hello, World!";
+});
