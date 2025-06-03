e90d86b6b4691a8ac07cb357290c9ea971f57b19
RohanY658 <rohanyampati@gmail.com>
2025-06-03 16:25:10 +0530

    Delete hello.js

--- hello.js
+++
@@ -1,6 +0,0 @@
-// hello.js
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("Hello, World!");
-  console.log("Hello, World!222");
-  document.getElementById("hello").innerHTML = "Hello, World!";
-});
